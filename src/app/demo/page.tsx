6e89c07eff573152381a27f7ee73fd556a3ead1e
Starrysit <shaurya.1229@gmail.com>
2026-05-17 19:50:17 +0530

    new update

---
+++ src/app/demo/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import styles from "./demo.module.css";
+import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/HeroSection";
+import AboutSection from "@/components/AboutSection";
+import SkillsSection from "@/components/SkillsSection";
+import ProjectsSection from "@/components/ProjectsSection";
+import ExperienceSection from "@/components/ExperienceSection";
+import AchievementsSection from "@/components/AchievementsSection";
+import ContactSection from "@/components/ContactSection";
+import Footer from "@/components/Footer";
+
+export default function DemoPage() {
+  return (
+    <div className={styles.demoRoot}>
+      <Navbar />
+      <main className={styles.containerOverride}>
+        <HeroSection />
+        <AboutSection />
+        <SkillsSection />
+        <ProjectsSection />
+        <ExperienceSection />
+        <AchievementsSection />
+        <ContactSection />
+        <Footer />
+      </main>
+    </div>
+  );
+}
